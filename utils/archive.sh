e3411be33bb28cbbd84da1732e2a86d76297a8b0
Andrei Maroz <amaroz@infoblox.com>
2022-03-31 14:13:16 +0300

    Updated archive script to ovveriding imageOffer, imagePublisher, imageSku and imageVersion variables in main template.

--- utils/archive.sh
+++ utils/archive.sh
@@ -12,14 +12,35 @@ function archive() {
 }
 
 function usage() {
-    echo -e "\nUsage: $0 accessPortal=<accessPortal> configServer=<configServer> notificationServer=<notificationServer> pid=<pid>\n"
+    echo -e "\nUsage: $0 accessPortal=<accessPortal> configServer=<configServer> imageOffer=<imageOffer> imagePublisher=<imagePublisher> imageSku=<imageSku> imageVersion=<imageVersion> notificationServer=<notificationServer> pid=<pid>"
+      echo -e "Required parameters:\n imageOffer\n imagePublisher\n imageSku\n imageVersion\n pid"
+    echo -e "Optional parameters:\n accessPortal\n configServer\n notificationServer\n"
 }
 
 # Patching mainTemplate.
 function patchMainTemplate() {
-    jq ".variables.hostSetup.accessPortal = \"${accessPortal}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    jq ".variables.hostSetup.configServer = \"${configServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    jq ".variables.hostSetup.notificationServer = \"${notificationServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    if [ -z ${accessPortal} ]; then
+        jq ".variables.hostSetup.accessPortal = \"${accessPortal}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    fi
+
+    if [ -z ${configServer} ]; then
+        jq ".variables.hostSetup.configServer = \"${configServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    fi
+
+    if [ -z ${notificationServer} ]; then
+        jq ".variables.hostSetup.notificationServer = \"${notificationServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    fi
+
+    # Removing hostSetup section in case if we not need override it.
+    if [ -z ${accessPortal} ] && [ -z ${configServer} ] && [ -z ${notificationServer} ]; then
+        jq "del(.variables.hostSetup)" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    fi
+
+    jq ".variables.imageOffer = \"${imageOffer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    jq ".variables.imagePublisher = \"${imagePublisher}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    jq ".variables.imageSku = \"${imageSku}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    jq ".variables.imageVersion = \"${imageVersion}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+
     jq ".resources[0].name = \"${pid}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
     
     echo "${MAIN_TEMPLATE} is patched."
@@ -34,7 +55,7 @@ do
     export "$KEY"="$VALUE"
 done
 
-if [ -z ${accessPortal} ] || [ -z ${configServer} ] || [ -z ${notificationServer} ] || [ -z ${pid} ]; then
+if [ -z ${imageOffer} ] || [ -z ${imagePublisher} ] || [ -z ${imageSku} ] || [ -z ${imageVersion} ] || [ -z ${pid} ]; then
     usage
     exit 0
 fi
