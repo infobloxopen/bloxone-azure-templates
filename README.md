cffcaf6fa74b0b703e1054b411a0fc15bbbf6324
Andrei Maroz <83007182+andrei-maroz@users.noreply.github.com>
2022-04-06 11:43:40 +0300

    Added script for preparing templates for QA team. (#11)
    * Added scripts for preparing templates for QA team.
    * Updated Readme file.
    * Removed unused variables.
    * Updated tooltip for proxy field.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # Bloxone Azure Templates
 
 ## Getting Started
+ 
+To prepare ARM templates for QA team use patch-for-qa.sh script from utils directory:
+```
+$ utils/patch-for-qa.sh accessPortal="" configServer="" notificationServer=""
+```
 
 To create zip archive use archive.sh script from utils directory:
 ```
-$ utils/archive.sh accessPortal="" configServer="" imageOffer="" imagePublisher="" imageSku="" imageVersion="" notificationServer="" pid=""
+$ utils/archive.sh imageOffer="" imagePublisher="" imageSku="" imageVersion="" trackingId=""
 ```
+
+You can get `imageOffer`, `imagePublisher`, `imageSku` and `imageVersion` in your virtual machine offer.
+
+You can get `trackingId` in your application offer.
--- utils/archive.sh
+++ utils/archive.sh
@@ -12,36 +12,20 @@ function archive() {
 }
 
 function usage() {
-    echo -e "\nUsage: $0 accessPortal=<accessPortal> configServer=<configServer> imageOffer=<imageOffer> imagePublisher=<imagePublisher> imageSku=<imageSku> imageVersion=<imageVersion> notificationServer=<notificationServer> pid=<pid>"
-    echo -e "Required parameters:\n imageOffer\n imagePublisher\n imageSku\n imageVersion\n pid"
-    echo -e "Optional parameters:\n accessPortal\n configServer\n notificationServer\n"
+    echo -e "\nUsage: $0 imageOffer=<imageOffer> imagePublisher=<imagePublisher> imageSku=<imageSku> imageVersion=<imageVersion> trackingId=<trackingId>"
+    echo -e "Required parameters:\n imageOffer\n imagePublisher\n imageSku\n imageVersion\n trackingId"
+    echo "You can get imageOffer, imagePublisher, imageSku and imageVersion in your virtual machine offer."
+    echo "You can get trackingId in your application offer."
 }
 
 # Patching mainTemplate.
 function patchMainTemplate() {
-    if [ ! -z ${accessPortal} ]; then
-        jq ".variables.hostSetup.accessPortal = \"${accessPortal}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    fi
-
-    if [ ! -z ${configServer} ]; then
-        jq ".variables.hostSetup.configServer = \"${configServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    fi
-
-    if [ ! -z ${notificationServer} ]; then
-        jq ".variables.hostSetup.notificationServer = \"${notificationServer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    fi
-
-    # Removing hostSetup section in case if we not need override it.
-    if [ -z ${accessPortal} ] && [ -z ${configServer} ] && [ -z ${notificationServer} ]; then
-        jq "del(.variables.hostSetup)" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    fi
-
     jq ".variables.imageOffer = \"${imageOffer}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
     jq ".variables.imagePublisher = \"${imagePublisher}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
     jq ".variables.imageSku = \"${imageSku}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
     jq ".variables.imageVersion = \"${imageVersion}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
 
-    jq ".resources[0].name = \"${pid}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
+    jq ".resources[0].name = \"${trackingId}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
     
     echo "${MAIN_TEMPLATE} is patched."
 }
@@ -55,7 +39,7 @@ do
     export "$KEY"="$VALUE"
 done
 
-if [ -z ${imageOffer} ] || [ -z ${imagePublisher} ] || [ -z ${imageSku} ] || [ -z ${imageVersion} ] || [ -z ${pid} ]; then
+if [ -z ${imageOffer} ] || [ -z ${imagePublisher} ] || [ -z ${imageSku} ] || [ -z ${imageVersion} ] || [ -z ${trackingId} ]; then
     usage
     exit 0
 fi
