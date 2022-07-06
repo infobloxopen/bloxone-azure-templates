affdeac415bec210975b5303559140a9d502a44e
Andrei Maroz <83007182+andrei-maroz@users.noreply.github.com>
2022-07-06 12:07:27 +0300

    [ATLAS-12985] Added workaround that allows to work with DropDown item as array always. (#16)

--- utils/patch-for-qa.sh
+++ utils/patch-for-qa.sh
@@ -7,8 +7,7 @@ CUSTOM_DATA_PARAMETER='{"type":"string","metadata":{"description":"CustomData fo
 CUSTOM_DATA_VM="[base64(if(empty(parameters('customData')), variables('customData'), parameters('customData')))]"
 CUSTOM_DATA_WO_HTTP_PROXY="[concat(variables('cloudConfig'), '  access_portal: ', variables('hostSetup').accessPortal, '\n  config_server: ', variables('hostSetup').configServer, '\n  notification_server: ', variables('hostSetup').notificationServer, '\n  jointoken: ', parameters('jointoken'), '\n')]"
 
-SG_SSH='{"type":"string","defaultValue":"false","allowedValues":["false","true"]}'
-SR="[if(equals(parameters('securityGroupSsh'), 'true'), concat(if(equals(parameters('securityGroupBootstrapUi'), 'true'), variables('securityRuleHtps'), json('[]')), variables('securityRuleSsh')), if(equals(parameters('securityGroupBootstrapUi'), 'true'), variables('securityRuleHtps'), json('[]')))]"
+SR="[if(contains(parameters('securityGroup'), 'ssh'), concat(if(contains(parameters('securityGroup'), 'bootstrapUi'), variables('securityRuleHtps'), json('[]')), variables('securityRuleSsh')), if(contains(parameters('securityGroup'), 'bootstrapUi'), variables('securityRuleHtps'), json('[]')))]"
 SR_SSH='[{"name":"SSH","properties":{"description":"Allow SSH","access":"Allow","destinationAddressPrefix":"*","destinationPortRange":"22","direction":"Inbound","priority":100,"protocol":"Tcp","sourceAddressPrefix":"*","sourcePortRange":"*"}}]'
 
 UI_CUSTOM_DATA_FIELD='[{"name":"customData","type":"Microsoft.Common.TextBox","label":"Custom data","toolTip":"Pass Custom Data for cloudinit.","multiLine":true,"constraints":{"required":false,"regex":"(?=.*).{0,100000}","validationMessage":"The Custom data cannot be more than 100000 characters."},"visible":"[equals(basics('"'"'injecting'"'"'), '"'"'Custom Data'"'"')]"}]'
@@ -17,7 +16,6 @@ UI_JOINTOKEN_VISIBLE="[equals(basics('injecting'), 'Jointoken')]"
 UI_INJECTING_FIELD='[{"name":"injecting","type":"Microsoft.Common.OptionsGroup","label":"Injecting","defaultValue":"Jointoken","toolTip":"Choose the type of injection.","constraints":{"allowedValues":[{"label":"Jointoken","value":"Jointoken"},{"label":"Custom Data","value":"Custom Data"}],"required":true},"visible":true}]'
 
 UI_SG_SSH_OPTION='[{"label":"SSH (22)","value":"ssh"}]'
-UI_SG_SSH_OUTPUT="[if(contains(steps('vmSettings').securityGroup, 'ssh'), 'true', 'false')]"
 
 function patchMainTemplate() {
     # Adding accessPortal, configServer, notificationServer, securityRuleSsh to variables section.
@@ -28,7 +26,6 @@ function patchMainTemplate() {
 
     # Adding customData, securityGroupSsh to parameters section.
     jq ".parameters.customData = ${CUSTOM_DATA_PARAMETER}" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
-    jq ".parameters.securityGroupSsh = ${SG_SSH}" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
 
     # Updating customDataWithoutHttpProxy, securityRules variables.
     jq ".variables.customDataWithoutHttpProxy = \"${CUSTOM_DATA_WO_HTTP_PROXY}\"" ${MAIN_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${MAIN_TEMPLATE}
@@ -54,7 +51,6 @@ function patchUITemplate() {
 
     # Adding scustomData, ecurityGroupSsh to output section.
     jq ".parameters.outputs.customData = \"${UI_CUSTOM_DATA_OUTPUT}\"" ${UI_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${UI_TEMPLATE}
-    jq ".parameters.outputs.securityGroupSsh = \"${UI_SG_SSH_OUTPUT}\"" ${UI_TEMPLATE} > tmp.$$.json && mv tmp.$$.json ${UI_TEMPLATE}
 }
 
 function usage() {
